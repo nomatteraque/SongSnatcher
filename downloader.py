6a554ffeb2cfbe2d84292edb11350559f04fea6e
Yusuf Kose <104000676+nomatteraque@users.noreply.github.com>
2026-05-22 19:55:52 +0100

    Add files via upload

--- downloader.py
+++ downloader.py
@@ -43,7 +43,7 @@ def download_audio(url):
         'quiet': False,
         'rm_cachedir': True,
         'extractor_args': {
-            'youtube': ['player_client=android,web_creator,ios']
+            'youtube': ['player_client=tv,web_creator,ios']
         },
     }
 
